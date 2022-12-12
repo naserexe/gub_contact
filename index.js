87518466e23c7612f9483d41d081f132483be602
Abdullah Naser <abdnaser.exe@gmail.com>
2022-12-12 14:45:33 +0600

    CORS added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const cors = require('cors');
 const contactRoutes = require('./routes/contactRoute');
 const connectDB = require('./config/db');
 const errorHandler = require('./middlewares/error');
@@ -8,6 +9,7 @@ dotenv.config()
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
