45e21494287e00f2ed64ec3ce8baf56929e1f22d
Abdullah Naser <abdnaser.exe@gmail.com>
2023-02-20 22:19:15 +0600

    Image now resized to 100*100px and some minor improvements

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,6 +1,7 @@
 const cloudinary = require('cloudinary');
 const fs = require('fs');
 const bcrypt = require('bcryptjs');
+const sharp = require('sharp');
 const asyncHandler = require('../middlewares/async');
 const contactModel = require('../models/contactModel');
 
@@ -21,13 +22,24 @@ exports.createContact = asyncHandler(async (req, res, next) => {
       api_key: process.env.API_KEY,
       api_secret: process.env.API_SECRET,
     });
+
+    await sharp(req.file.path)
+    .resize(100, 100)
+    .toFile(`./temp/pro-${req.file.filename}`);
   
-    photo = await imageUpload.uploader.upload(req.file.path);
+    photo = await imageUpload.uploader.upload(`./temp/pro-${req.file.filename}`);
     if (!photo){
       throw new Error('Failed to save profile photo');
     }
     req.body.photo = photo.secure_url;
+
     fs.unlink(`./temp/${req.file.filename}`, (err) => {
+      if (err) {
+        console.error('Failed to clean temp file')
+      }
+    });
+
+    fs.unlink(`./temp/pro-${req.file.filename}`, (err) => {
       if (err) {
         console.error('Failed to clean temp file')
       }
@@ -58,6 +70,10 @@ exports.updateContact = asyncHandler(async (req, res, next) => {
     throw new Error('No contact found')
   }
 
+  if(!req.user.isAdmin && req.user._id.toString() !== isContactExists._id.toString()){
+    return res.status(403).json({ success: false, message: 'You are not authorized to perform this operation'});
+  }
+
   let photo;
 
   if(req?.file?.path){
@@ -69,12 +85,28 @@ exports.updateContact = asyncHandler(async (req, res, next) => {
       api_secret: process.env.API_SECRET,
     });
 
-    photo = await imageUpload.uploader.upload(req.file.path);
+    await sharp(req.file.path)
+    .resize(100, 100)
+    .toFile(`./temp/pro-${req.file.filename}`);
+
+    photo = await imageUpload.uploader.upload(`./temp/pro-${req.file.filename}`);
     if (!photo){
       throw new Error('Failed to save profile photo');
     }
 
     req.body.photo = photo.secure_url;
+
+    fs.unlink(`./temp/${req.file.filename}`, (err) => {
+      if (err) {
+        console.error('Failed to clean temp file')
+      }
+    });
+
+    fs.unlink(`./temp/pro-${req.file.filename}`, (err) => {
+      if (err) {
+        console.error('Failed to clean temp file')
+      }
+    });
   }
 
   if(!req.user.isAdmin){
